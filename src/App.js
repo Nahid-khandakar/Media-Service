075cb451dbf6beba159cdc8290985e5d745cebcc
Nahid-khandakar <nahidkhandakar.11010@gmail.com>
2022-04-17 15:49:48 +0600

    update navbar and slove background image problem

--- src/App.js
+++ src/App.js
@@ -1,11 +1,23 @@
+import { Route, Routes } from 'react-router-dom';
 import './App.css';
+import About from './component/About/About';
+import Blog from './component/Blog/Blog';
+import Home from './component/Home/Home';
+
+import Header from './component/Shared/Header/Header';
 
 
 function App() {
   return (
     <div>
 
-      <h1>hello</h1>
+      <Header></Header>
+      <Routes>
+        <Route path='/' element={<Home></Home>}></Route>
+        <Route path='/home' element={<Home></Home>}></Route>
+        <Route path='/blog' element={<Blog></Blog>}></Route>
+        <Route path='/about' element={<About></About>}></Route>
+      </Routes>
     </div>
   );
 }
--- src/component/Home/Home.js
+++ src/component/Home/Home.js
@@ -1,10 +1,16 @@
 import React from 'react';
 import './Home.css'
+import banner from '../../images/banner.jpg'
 
 const Home = () => {
     return (
-        <div>
-            <h1>this is home div</h1>
+
+        <div className='px-3'>
+            <div className='bg-img' style={{
+                backgroundImage: `url(${banner})`
+            }}>
+
+            </div>
         </div>
     );
 };
