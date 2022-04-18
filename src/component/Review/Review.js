31fbc31abc83161a40daf413a61346a0463c9dd0
Nahid-khandakar <nahidkhandakar.11010@gmail.com>
2022-04-18 07:44:45 +0600

    review updated and react icon add

--- src/component/Review/Review.js
+++ src/component/Review/Review.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import './Review.css'
-
-
+import Rating from 'react-rating';
+import { FaStar } from 'react-icons/fa';
 
 
 
@@ -10,11 +10,20 @@ const Review = ({ review }) => {
     return (
 
         <div className="col col-lg-3 col-12">
-            <div className='card review-card rounded my-3 ' >
+            <div className='card review-card rounded my-5 ' >
                 <div className="card-body ">
                     <img src={image} alt="" className='img-fluid rounded-2 w-100 d-block mx-auto review-card-img shadow-sm' />
                     <h3 className='mt-3 text-center'>{name}</h3>
                     <p className='review-card-text'>{userReview}</p>
+
+                    <div className='text-center'>
+                        <Rating
+                            initialRating={rating}
+                            emptySymbol={<FaStar></FaStar>}
+                            fullSymbol={<FaStar style={{ color: 'goldenrod' }}></FaStar>}
+                            readonly
+                        ></Rating>
+                    </div>
                 </div>
             </div>
         </div>
