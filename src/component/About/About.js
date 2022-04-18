ebb2832dea2160a9965cce70ecf376b83da8c6a3
Nahid-khandakar <nahidkhandakar.11010@gmail.com>
2022-04-18 06:00:28 +0600

    about section updated

--- src/component/About/About.js
+++ src/component/About/About.js
@@ -5,7 +5,7 @@ import aboutMe from '../../images/aboutMe.jpg'
 const About = () => {
     return (
 
-        <div className="container mt-5 p-2">
+        <div className="container mt-3 p-2">
             <div className="col-md-4 mx-auto">
                 <div className="card profile-card">
                     <div className="card-img-block">
@@ -13,7 +13,7 @@ const About = () => {
                     </div>
                     <div className="card-body pt-0">
                         <h5 className="card-title">Nahid Khandakar</h5>
-                        <p className="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+                        <p className="card-text">As a Web developer my prime goal is design Web pages to effectively and be artistically . I also have an interest in database management , UI/UX design and cyber security. I want to work on these field in the future.</p>
 
                     </div>
                 </div>
